1274e58aec901359f34d2ebf1e5a9a130cc3e770
maxwen <max.weninger@gmail.com>
2017-06-15 04:41:15 -0300

    Add iconpack support Support for icon pack ported from: https://github.com/Deletescape-Media/Lawnchair
    Launcher3: rework icon pack support
    replace icons already on the DB level
    
    Launcher3: full icon pack support
    Blatantly copied from OmniSwitch :)
    
    Add custom preference for icon packs
    
    based on
    https://github.com/Deletescape-Media/Lawnchair/commit/8a0a79d66c809294ea1c7a95b3e6f66cd7de4e10
    
    Change-Id: I40546e03f61520780763e08fe44f9618f3503095
    Signed-off-by: AudioGod <audiogod.adi@gmail.com>

--- src/com/android/launcher3/Launcher.java
+++ src/com/android/launcher3/Launcher.java
@@ -4150,6 +4150,11 @@ public class Launcher extends BaseActivity
                 // it will initialize the rotation preference again.
                 finish();
             }
+            if ("pref_iconPackPackage".equals(key)) {
+                mModel.clearIconCache();
+                mModel.forceReload();
+                mOnResumeNeedsLoad = true;
+            }
         }
     }
 }
