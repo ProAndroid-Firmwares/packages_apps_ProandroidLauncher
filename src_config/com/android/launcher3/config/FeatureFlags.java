3bd0a6f89534083c027e2d0221d12caffdb0513e
AudioGod <audiogod.adi@gmail.com>
2017-10-24 12:48:42 +0530

    Enforce Lagacy Icon Masking & Disable icon Shadow
    Change-Id: I790189e82f262c9cf29e01d25e6fbceda53b668f
    Signed-off-by: AudioGod <audiogod.adi@gmail.com>

--- src_config/com/android/launcher3/config/FeatureFlags.java
+++ src_config/com/android/launcher3/config/FeatureFlags.java
@@ -46,9 +46,9 @@ public final class FeatureFlags {
     // When enabled icons are badged with the number of notifications associated with that app.
     public static final boolean BADGE_ICONS = true;
     // When enabled, icons not supporting {@link AdaptiveIconDrawable} will be wrapped in this class.
-    public static final boolean LEGACY_ICON_TREATMENT = false;
+    public static final boolean LEGACY_ICON_TREATMENT = true;
     // When enabled, adaptive icons would have shadows baked when being stored to icon cache.
-    public static final boolean ADAPTIVE_ICON_SHADOW = true;
+    public static final boolean ADAPTIVE_ICON_SHADOW = false;
     // When enabled, app discovery will be enabled if service is implemented
     public static final boolean DISCOVERY_ENABLED = false;
     // enable/disable adaptive icon support
